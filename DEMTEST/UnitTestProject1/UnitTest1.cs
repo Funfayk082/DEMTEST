915fdd35a2dae0e7243fc787d19e996a70aa627e
123 <123>
2024-05-06 09:31:53 +0300

    feat: Реализовал сам не знаю что

---
+++ DEMTEST/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LibraryName;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod15()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod12()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod13()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+        [TestMethod]
+        public void TestMethod14()
+        {
+            Assert.AreEqual(Class1.TestMethod(1, 2), Class1.TestMethod(2, 1));
+        }
+
+    }
+}
